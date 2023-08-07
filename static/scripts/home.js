31fad7817d89a1b3a8131c1a78445c5bb8568074
AnupamMisra <innocentangelfromhell@gmail.com>
2023-08-07 15:04:04 +0530

    UI changes

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -4,13 +4,17 @@ document.getElementById("button1").addEventListener("click", function() {
 });
 
 document.getElementById("button2").addEventListener("click", function() {
-  window.location.href = "parse_resume";
+  window.location.href = "show_jd";
 });
 
 document.getElementById("button3").addEventListener("click", function() {
-  window.location.href = "recommend_candidate";
+  window.location.href = "parse_resume";
 });
 
 document.getElementById("button4").addEventListener("click", function() {
+  window.location.href = "recommend_candidate";
+});
+
+document.getElementById("button5").addEventListener("click", function() {
   window.location.href = "available_candidates";
 });
