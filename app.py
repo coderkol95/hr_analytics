ef24ed06958de776c8b27b7c1c01ed2d8ee26f73
Krishnendu <kd.datascience@gmail.com>
2023-08-07 16:57:56 +0530

    app.py

--- app.py
+++ app.py
@@ -277,10 +277,12 @@ def shortlist_candidates():
             emails = request.form.getlist('email_checkbox')
             for email in emails:
                     try:
-                        jd = "It needs to come from somewhere"
+                        # jd = "It needs to come from somewhere"
                         candidate_credentials_obj = CandidateCredentials(db_config)
                         ## Creating PW for the candidate to login in test portal
                         candidate_credentials_obj.create_candidate_credentials(email)
+                        candidate_qna.update_one({"email": email}, 
+                                                 {"$set": {"status": "Assessment Initiated"}})
 
                         resume_qna_obj = ResumeQnAGenerator(email)
                         resume_objective_question_prompt = resume_qna_obj.promptMCQs()
@@ -293,22 +295,20 @@ def shortlist_candidates():
                             resume_subjective_questions = resume_qna_obj.askGPT(resume_subjective_question_prompt)
                             resume_qna_obj.insertDescriptiveQAforCandidate(resume_subjective_questions)
 
-                        jd_qna_obj = JDQnAGenerator(email, jd)
-                        jd_objective_question_prompt = jd_qna_obj.promptMCQsfromJD()
-                        jd_subjective_question_prompt =  jd_qna_obj.promptDescriptiveQuestionsfromJD()
+                        # jd_qna_obj = JDQnAGenerator(email, jd)
+                        # jd_objective_question_prompt = jd_qna_obj.promptMCQsfromJD()
+                        # jd_subjective_question_prompt =  jd_qna_obj.promptDescriptiveQuestionsfromJD()
 
-                        if jd_objective_question_prompt:
-                            jd_objective_questions = jd_qna_obj.askGPT(jd_objective_question_prompt)
-                            jd_qna_obj.insertMCQAforCandidate(jd_objective_questions)
-                        if jd_subjective_question_prompt:
-                            jd_subjective_questions = jd_qna_obj.askGPT(jd_subjective_question_prompt)
-                            jd_qna_obj.insertDescriptiveQAforCandidate(jd_subjective_questions)
-
+                        # if jd_objective_question_prompt:
+                        #     jd_objective_questions = jd_qna_obj.askGPT(jd_objective_question_prompt)
+                        #     jd_qna_obj.insertMCQAforCandidate(jd_objective_questions)
+                        # if jd_subjective_question_prompt:
+                        #     jd_subjective_questions = jd_qna_obj.askGPT(jd_subjective_question_prompt)
+                        #     jd_qna_obj.insertDescriptiveQAforCandidate(jd_subjective_questions)
 
                     except Exception as e:
-                        return jsonify(f"Cannot create Resume/JD questions for {email}", e)
-                    
-                 
+                        return jsonify(f"Cannot create Resume questions for {email}", e)
+                                     
     return redirect("/")
 
 @app.route("/recruitment_journey", methods=['GET','POST'])
